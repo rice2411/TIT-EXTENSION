024ae29b846db628d253eb7d13f8ccee703c7cf9
PhucLH <LongDH@twinger.vn>
2024-07-04 14:55:43 +0700

    feat/TIT-003 Create Base UI for default PopUp

---
+++ scripts/welcome.js
@@ -0,0 +1,13 @@
+
+function displayCourseWarningComponent() {
+    var newNode = document.createElement('div');
+    newNode.innerHTML = '<p class="alert alert-success"> Bọn mình là <b>TIT EXTENSION</b>, tiện ích này sẽ giúp 4-5 năm học Đại học của bạn dễ dàng hơn </p>'
+    const location = document.getElementById('ajax-loader')
+    location.parentNode.insertBefore(newNode, location)
+}
+
+window.addEventListener('load', function () {
+    displayCourseWarningComponent()
+});
+
+
