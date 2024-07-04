c8da3d4d9f55aeec972bcff000d9b6e895824720
hohang2411 <20t6070009.husc.edu.vn>
2024-07-04 19:35:08 +0700

    feat/TIT-004 Add alert refresh page

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,25 +1,24 @@
-
 const displayCourseWarningComponent = () => {
-    var newNode = document.createElement('div');
-    newNode.innerHTML = '<p class="alert alert-success"> Bọn mình là <b>TIT EXTENSION</b>, tiện ích này sẽ giúp 4-5 năm học Đại học của bạn dễ dàng hơn </p>'
-    const location = document.getElementById('ajax-loader')
-    location.parentNode.insertBefore(newNode, location)
-}
+  var newNode = document.createElement("div");
+  newNode.innerHTML =
+    '<p class="alert alert-success"> Bọn mình là <b>TIT EXTENSION</b>, tiện ích này sẽ giúp 4-5 năm học Đại học của bạn dễ dàng hơn </p>';
+  const location = document.getElementById("ajax-loader");
+  location.parentNode.insertBefore(newNode, location);
+};
 
 const gettPermissionNotification = async () => {
-    const clientPermission = localStorage.getItem('isCanNoti')
-    const { newPermission } = await chrome.storage.sync.get('newPermission')
-    if (!clientPermission) {
-        chrome.storage.sync.set({ 'isCanNoti': 1 });
-        localStorage.setItem('isCanNoti', 1)
-    } else {
-        chrome.storage.sync.set({ 'isCanNoti': newPermission ?? +clientPermission });
-    }
-}
+  const clientPermission = localStorage.getItem("isCanNoti");
+  const { newPermission } = await chrome.storage.sync.get("newPermission");
+  if (!clientPermission) {
+    chrome.storage.sync.set({ isCanNoti: 1 });
+    localStorage.setItem("isCanNoti", 1);
+  } else {
+    chrome.storage.sync.set({ isCanNoti: newPermission ?? +clientPermission });
+    localStorage.setItem("isCanNoti", newPermission ?? +clientPermission);
+  }
+};
 
-window.addEventListener('load', () => {
-    displayCourseWarningComponent()
-    gettPermissionNotification()
+window.addEventListener("load", () => {
+  displayCourseWarningComponent();
+  gettPermissionNotification();
 });
-
-
